db0d6de6c881560dcffad23571dbba4426186922
Alexkcshing <alexkcshing@gmail.com>
2019-01-15 18:28:22 +0000

    adding fahrenheit to celsius test

--- specs/ruby_functions_spec.rb
+++ specs/ruby_functions_spec.rb
@@ -86,15 +86,16 @@ class FunctionsTest < MiniTest::Test
   # end
 
   #Given the radius of a sphere calculate the volume
-  def test_volume_of_sphere()
-    result = volume_of_sphere(3)
-    assert_equal(113, result)
-  end
-  #
-  # #Given a value in farenheit, convert this into celsius.
-  # def test_fahrenheit_to_celsius()
-  #   #add test code here
+  # def test_volume_of_sphere()
+  #   result = volume_of_sphere(3)
+  #   assert_equal(113, result)
   # end
+  #
+  #Given a value in farenheit, convert this into celsius.
+  def test_fahrenheit_to_celsius()
+    result = fahrenheit_to_celsius(60)
+    assert_equal(15, result)
+  end
 
 
 end
