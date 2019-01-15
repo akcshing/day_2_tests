5401e61b313d9f6f68d624ccf9ef7b4af26ba138
Alexkcshing <alexkcshing@gmail.com>
2019-01-15 17:58:06 +0000

    change shortmonth to return first 3 chars of fullmonth

--- month_test.rb
+++ month_test.rb
@@ -1,31 +1,6 @@
-def number_to_short_month_name(month)
-  case month
-    when 1
-      return "Jan"
-    when 2
-      return "Feb"
-    when 3
-      return "Mar"
-    when 4
-      return "Apr"
-    when 5
-      return "May"
-    when 6
-      return "Jun"
-    when 7
-      return "Jul"
-    when 8
-      return "Aug"
-    when 9
-      return "Sep"
-    when 10
-      return "Oct"
-    when 11
-      return "Nov"
-    when 12
-      return "Dec"
-  end
-end
+month = "January"
 
 
-first_month_string = number_to_short_month_name( 1 )
+shortmonth = month[0...3]
+
+p shortmonth
